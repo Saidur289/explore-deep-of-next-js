214936284ad68584d2207654e318590d7c2059f2
Saidur289 <saidur.riaz1@gmailcomm>
2025-07-30 14:35:33 +0600

    create dashboard route

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        Navbar from layout will all route
         {children}
       </body>
     </html>
